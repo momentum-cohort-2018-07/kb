3d1211212c548c7fa61b94e3cdb858951d8e7594
Clinton Dreisbach <clinton@dreisbach.us>
2018-08-23 14:25:07 -0400

    Add changing book status + event delegation

--- w4--frontend/examples/book-track/book-track.js
+++ w4--frontend/examples/book-track/book-track.js
@@ -4,22 +4,6 @@ import './book-track.css'
 import request from 'superagent'
 import uuid from 'uuid/v4'
 
-request.get('http://localhost:3000/books')
-  .then(response => {
-    let books = response.body
-
-    for (let currentStatus of ['reading', 'read', 'to-read']) {
-      let list = el('ul')
-      for (let book of books.filter(book => book.status === currentStatus)) {
-        let bookLi = createBookDOM(book)
-        list.appendChild(bookLi)
-      }
-
-      getId(currentStatus).innerHTML = ''
-      getId(currentStatus).appendChild(list)
-    }
-  })
-
 getId('add-book-button').addEventListener('click', event => {
   hide('add-book-button')
   show('new-book-form')
@@ -48,6 +32,39 @@ getId('new-book-form').addEventListener('submit', event => {
     })
 })
 
+// Listen for any clicks anywhere inside the div with id books.
+// If the click is on a delete link, delete the book.
+// If the click is on a promote button, promote the book.
+getId('books').addEventListener('click', event => {
+  if (event.target && event.target.classList.contains('delete-book')) {
+    deleteBook(event.target.dataset.bookId)
+  }
+
+  if (event.target && event.target.classList.contains('promote-button')) {
+    promoteBook(
+      event.target.parentElement.dataset.bookId,
+      event.target.dataset.nextStatus)
+  }
+})
+
+function getBooks () {
+  request.get('http://localhost:3000/books')
+    .then(response => {
+      let books = response.body
+
+      for (let currentStatus of ['reading', 'read', 'to-read']) {
+        let list = el('ul')
+        for (let book of books.filter(book => book.status === currentStatus)) {
+          let bookLi = createBookDOM(book)
+          list.appendChild(bookLi)
+        }
+
+        getId(currentStatus).innerHTML = ''
+        getId(currentStatus).appendChild(list)
+      }
+    })
+}
+
 function addBookToPage (book) {
   let section = getId(book.status)
   let list = section.querySelector('ul')
@@ -59,23 +76,45 @@ function createBookDOM (book) {
   let bookLi = el('li')
   bookLi.classList.add('book')
   bookLi.id = `book-${book.id}`
+  bookLi.dataset.bookId = book.id
   bookLi.innerHTML = `<i>${book.title}</i>, ${book.authors.join(', ')}`
+
+  if (book.status !== 'read') {
+    let promoteButton = el('button')
+    promoteButton.classList.add('button-xs', 'promote-button')
+    promoteButton.style.marginLeft = '0.5rem'
+    if (book.status === 'to-read') {
+      promoteButton.innerText = 'Mark as reading'
+      promoteButton.dataset.nextStatus = 'reading'
+    } else {
+      promoteButton.innerText = 'Mark as read'
+      promoteButton.dataset.nextStatus = 'read'
+    }
+    bookLi.appendChild(promoteButton)
+  }
+
   let deleteLink = el('a')
   deleteLink.href = '#'
   deleteLink.style.paddingLeft = '0.5rem'
-  deleteLink.classList.add('text-danger')
+  deleteLink.classList.add('text-danger', 'delete-book')
+  deleteLink.dataset.bookId = book.id
   deleteLink.innerText = 'x'
-  deleteLink.addEventListener('click', event => {
-    deleteBook(book)
-  })
   bookLi.appendChild(deleteLink)
   return bookLi
 }
 
-function deleteBook (book) {
-  request.delete(`http://localhost:3000/books/${book.id}`)
+function deleteBook (bookId) {
+  request.delete(`http://localhost:3000/books/${bookId}`)
     .then(response => {
-      getId(`book-${book.id}`).remove()
+      getId(`book-${bookId}`).remove()
+    })
+}
+
+function promoteBook (bookId, status) {
+  request.patch(`http://localhost:3000/books/${bookId}`)
+    .send({'status': status})
+    .then(response => {
+      getBooks()
     })
 }
 
@@ -94,3 +133,5 @@ function show (id) {
 function hide (id) {
   getId(id).style.display = 'none'
 }
+
+getBooks()
