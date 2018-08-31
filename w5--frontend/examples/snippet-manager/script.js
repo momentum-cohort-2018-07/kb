0b410a0e02e82852a610dcf7aba2a5563018127e
Clinton Dreisbach <clinton@dreisbach.us>
2018-08-31 16:37:00 -0400

    Further decomplection of snippet manager

--- w5--frontend/examples/snippet-manager/script.js
+++ w5--frontend/examples/snippet-manager/script.js
@@ -1,6 +1,42 @@
 import 'shoelace-css/dist/shoelace.css'
 import './styles.css'
 
-import app from './src/app'
+import Router from 'vanilla-router'
+import store from './src/store'
+import views from './src/views'
+
+const router = new Router({
+  mode: 'history'
+})
+
+router.add('', () => {
+  store.changeView('main')
+})
+
+router.add('edit/{id}', (id) => {
+  store.changeView('editSnippet', {id: id})
+})
+
+function mount (child) {
+  const root = document.getElementById('root')
+  if (root.firstChild) {
+    root.replaceChild(child, root.firstChild)
+  } else {
+    root.appendChild(child)
+  }
+}
+
+const app = {
+  router: router,
+  store: store,
+  views: views,
+  start: () => {
+    app.store.router = router
+    app.store.refreshFn = () => mount(views.app())
+    app.router.addUriListener()
+    app.router.check()
+    app.store.retrieveSnippets()
+  }
+}
 
 app.start()
